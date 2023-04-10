cceaf3bdf7fad1cb4bd0774002c4754a32ae6ca3
Anna DeBrunner <anna_debrunner@mckinsey.com>
2023-04-10 10:25:06 -0500

    Add lat/long filter to basic_cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,10 @@ def go(args):
     logger.info("Converting column last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Filter latitude and longitude
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy() 
+
     logger.info("Saving cleaned dataset to clean_sample.csv")
     df.to_csv("clean_sample.csv", index=False)
 
